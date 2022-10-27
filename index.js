ed1fe023ce6a3928fba571153a16f02ed4da365e
Sherzodbek1717 <113887151+Sherzodbek1717@users.noreply.github.com>
2022-10-27 02:19:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// This is my first JavaScript code!
+console.log('Hello world')
+let name = 'Sherzodbek';
+console.log(name);
+
+// Cannot be a reserved keyword
+// should be meaningful
+what's up 
