7e3b5381b95ffd9faf9f6c9ca6726a8a26de86cc
Sherzodbek1717 <113887151+Sherzodbek1717@users.noreply.github.com>
2022-10-29 06:36:54 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// This is my first JavaScript code!
-console.log('Hello world')
-let name = 'Sherzodbek';
-console.log(name);
-
-// Cannot be a reserved keyword
-// should be meaningful
-what's up 
